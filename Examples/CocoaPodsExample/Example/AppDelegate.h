76b9d42d9dc1c6821e2b875be0b234d712d63ca0
Peter Jenkins <peterjenkins@users.noreply.github.com>
2016-04-29 16:02:26 -0400

    Add Examples

---
+++ Examples/CocoaPodsExample/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Peter Jenkins on 4/29/16.
+//  Copyright © 2016 mParticle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/CocoaPodsExample/Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Example
+//
+//  Created by Peter Jenkins on 4/29/16.
+//  Copyright © 2016 mParticle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
