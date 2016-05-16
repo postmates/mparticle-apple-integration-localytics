c5c5bdf678198ad3fcfe783aac3b1b381f79acb4
Sid Raval <sid@thoughtbot.com>
2016-05-16 17:37:21 -0400

    Carthage compatible Localytics kit for mParticle

---
+++ mParticle-Localytics/mParticle-Localytics.h
@@ -0,0 +1,20 @@
+//
+//  mParticle-Localytics.h
+//  mParticle-Localytics
+//
+//  Created by Sid Raval on 5/16/16.
+//  Copyright © 2016 mParticle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mParticle-Localytics.
+FOUNDATION_EXPORT double mParticle_LocalyticsVersionNumber;
+
+//! Project version string for mParticle-Localytics.
+FOUNDATION_EXPORT const unsigned char mParticle_LocalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mParticle_Localytics/PublicHeader.h>
+
+#import <mParticle_Localytics/MPKitLocalytics.h>
+#import <Localytics.h>
